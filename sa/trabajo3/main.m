fa822bb1bba3277c2c369edcb8aeacde86386a94
charmosaa <272682@student.pwr.edu.pl>
2026-04-07 14:37:39 +0200

    feat: small refactor

--- sa/trabajo3/main.m
+++ sa/trabajo3/main.m
@@ -1,212 +1,198 @@
-%% 0. Inicio
+%% 0. Setup
 load('mapa_obstaculos_final.mat');
-umbral = 0.03; 
-mapaBinarizada = gridMapNorm > umbral;
-mapa = binaryOccupancyMap(mapaBinarizada, 1/cellSize);
+threshold = 0.03; 
+mapBin = gridMapNorm > threshold;
+map = binaryOccupancyMap(mapBin, 1/cellSize);
+
 figure;
-show(mapa);
+show(map);
 title('Binarized Occupancy Map');
 
-%% 1. Planificación sin obstáculos
-start1_world = [2, 15];
-goal1_world = [12, 15];
+%% 1. Path planning without obstacles
+start1World = [2, 15];
+goal1World = [12, 15];
+start1Grid = world2grid(map, start1World);
+goal1Grid = world2grid(map, goal1World);
 
-start1_grid = world2grid(mapa, start1_world);
-goal1_grid = world2grid(mapa, goal1_world);
+planner1 = plannerAStarGrid(map);
+[path1Grid, debugInfo1] = plan(planner1, start1Grid, goal1Grid);
+path1World = grid2world(map, path1Grid);
 
-planner1 = plannerAStarGrid(mapa);
-[path1_grid, debugInfo1] = plan(planner1, start1_grid, goal1_grid);
+plotAndSave(map, path1World, start1World, goal1World, ...
+            '1. Ruta A* sin obstáculos', '1_imagen.png', '1_resultado', debugInfo1);
 
-path1_world = grid2world(mapa, path1_grid);
+%% 2. Path planning with obstacles
+start2World = [1, 2];
+goal2World = [12, 12];
+start2Grid = world2grid(map, start2World);
+goal2Grid = world2grid(map, goal2World);
 
-figure;
-show(mapa);
-hold on;
-plot(path1_world(:,1), path1_world(:,2), 'c-', 'LineWidth', 2);
-plot(start1_world(1), start1_world(2), 'go', 'MarkerSize', 8, 'MarkerFaceColor', 'g');
-plot(goal1_world(1), goal1_world(2), 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r');
-title('1. Ruta A* sin obstáculos');
-legend('', 'Start', 'Meta');
+planner2 = plannerAStarGrid(map);
+[path2Grid, debugInfo2] = plan(planner2, start2Grid, goal2Grid);
+path2World = grid2world(map, path2Grid);
 
-save('actividad_1_resultado.mat', 'path1_world', 'debugInfo1');
-saveas(gcf, 'actividad_1_imagen.png');
-fprintf('Guardado imagen para el punto 1.\n');
+plotAndSave(map, path2World, start2World, goal2World, ...
+            '2. Ruta A* con obstáculos', '2_imagen.png', '2_resultado', debugInfo2);
 
-%% 2. Planificación con obstáculos
-start2_world = [1, 2];
-goal2_world = [12, 12];
+%% 3. Inflating obstacle boundaries
+mapInflated = copy(map);
+vehicleRadius = 0.2;
+inflate(mapInflated, vehicleRadius);
 
-start2_grid = world2grid(mapa, start2_world);
-goal2_grid = world2grid(mapa, goal2_world);
+start3World = [1, 2];
+goal3World = [12, 12];
+% Other points tested for point 5 validation:
+% start3World = [12, 2]; goal3World = [1, 13];
+% start3World = [12, 2]; goal3World = [12, 12];
+% start3World = [15.5, 5.5]; goal3World = [1, 1];
 
-planner2 = plannerAStarGrid(mapa);
-[path2_grid, debugInfo2] = plan(planner2, start2_grid, goal2_grid);
+start3Grid = world2grid(mapInflated, start3World);
+goal3Grid = world2grid(mapInflated, goal3World);
 
-path2_world = grid2world(mapa, path2_grid);
+planner3 = plannerAStarGrid(mapInflated);
+[path3Grid, debugInfo3] = plan(planner3, start3Grid, goal3Grid);
+path3World = grid2world(mapInflated, path3Grid);
 
-figure;
-show(mapa);
-hold on;
-plot(path2_world(:,1), path2_world(:,2), 'c-', 'LineWidth', 2);
-plot(start2_world(1), start2_world(2), 'go', 'MarkerSize', 8, 'MarkerFaceColor', 'g');
-plot(goal2_world(1), goal2_world(2), 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r');
-title('2. Ruta A* con obstáculos');
-legend('', 'Start', 'Meta');
+plotAndSave(mapInflated, path3World, start3World, goal3World, ...
+            '3. Ruta A* con obstáculos inflados', '3_imagen.png', '3_resultado', debugInfo3);
 
-save('actividad_2_resultado.mat', 'path2_world', 'debugInfo2');
-saveas(gcf, 'actividad_2_imagen.png');
-fprintf('Guardado imagen para el punto 2.\n');
+distance2 = sum(sqrt(sum(diff(path2World).^2, 2)));
+distance3 = sum(sqrt(sum(diff(path3World).^2, 2)));
+fprintf('Distancia de la ruta 2 (sin inflar): %.2f m\n', distance2);
+fprintf('Distancia de la ruta 3 (inflada): %.2f m\n', distance3);
 
-%% 3. Inflando los contornos de los obstáculos
-mapaInflada = copy(mapa);
+%% 4. Influence of cell size
+% Large cells
+cellSizeLarge = 0.2;
+mapBinLarge = imresize(mapBin, 0.1/cellSizeLarge, 'nearest');
+mapLarge = binaryOccupancyMap(mapBinLarge, 1/cellSizeLarge);
 
-radio_vehiculo = 0.2;
-inflate(mapaInflada, radio_vehiculo);
-
-start3_world = [1, 2];
-goal3_world = [12, 12];
-% otros puntos que se ha probado para comprobar actividad 5
-% start3_world = [12, 2];
-% goal3_world = [1, 13];
-% start3_world = [12, 2];
-% goal3_world = [12, 12];
-% start3_world = [15.5, 5.5];
-% goal3_world = [1, 1];
-
-
-start3_grid = world2grid(mapaInflada, start3_world);
-goal3_grid = world2grid(mapaInflada, goal3_world);
-
-planner3 = plannerAStarGrid(mapaInflada);
-[path3_grid, debugInfo3] = plan(planner3, start3_grid, goal3_grid);
-
-path3_world = grid2world(mapaInflada, path3_grid);
-
-figure;
-show(mapaInflada);
-hold on;
-plot(path3_world(:,1), path3_world(:,2), 'c-', 'LineWidth', 2);
-plot(start3_world(1), start3_world(2), 'go', 'MarkerSize', 8, 'MarkerFaceColor', 'g');
-plot(goal3_world(1), goal3_world(2), 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r');
-title('3. Ruta A* con obstáculos inflados');
-legend('', 'Start', 'Meta');
-
-save('actividad_3_resultado.mat', 'path3_world', 'debugInfo3');
-saveas(gcf, 'actividad_3_imagen.png');
-fprintf('Guardado imagen para el punto 3.\n');
-
-distancia2 = sum(sqrt(sum(diff(path2_world).^2, 2)));
-distancia3 = sum(sqrt(sum(diff(path3_world).^2, 2)));
-fprintf('Distancia de la ruta 2 (sin inflar): %.2f m\n', distancia2);
-fprintf('Distancia de la ruta 3 (inflada): %.2f m\n', distancia3);
-
-
-%% 4. Influencia del tamaño de las celdas
-cellSize_grande = 0.2;
-mapaBinarizada_grande = imresize(mapaBinarizada, 0.1/cellSize_grande, 'nearest');
-mapa_gran = binaryOccupancyMap(mapaBinarizada_grande, 1/cellSize_grande);
-
-start4_grande_grid = world2grid(mapa_gran, start3_world);
-goal4_grande_grid = world2grid(mapa_gran, goal3_world);
-planner4_grande = plannerAStarGrid(mapa_gran);
+start4LargeGrid = world2grid(mapLarge, start3World);
+goal4LargeGrid = world2grid(mapLarge, goal3World);
+planner4Large = plannerAStarGrid(mapLarge);
 
 tic;
-[path4_grande_grid, debugInfo4_grande] = plan(planner4_grande, start4_grande_grid, goal4_grande_grid);
-tiempo_grandes = toc;
-fprintf('Tiempo de cómputo para celdas GRANDES (0.2m): %.4f segundos\n', tiempo_grandes);
+[path4LargeGrid, debugInfo4Large] = plan(planner4Large, start4LargeGrid, goal4LargeGrid);
+timeLarge = toc;
+fprintf('Tiempo de cómputo para celdas GRANDES (0.2m): %.4f s\n', timeLarge);
 
-cellSize_peq = 0.02;
-mapaBinarizada_pequena = imresize(mapaBinarizada, 0.1/cellSize_peq, 'nearest');
-mapa_peq = binaryOccupancyMap(mapaBinarizada_pequena, 1/cellSize_peq);
+% Small cells
+cellSizeSmall = 0.02;
+mapBinSmall = imresize(mapBin, 0.1/cellSizeSmall, 'nearest');
+mapSmall = binaryOccupancyMap(mapBinSmall, 1/cellSizeSmall);
 
-start4_pequena_grid = world2grid(mapa_peq, start3_world);
-goal4_pequena_grid = world2grid(mapa_peq, goal3_world);
-planner4_pequena = plannerAStarGrid(mapa_peq);
+start4SmallGrid = world2grid(mapSmall, start3World);
+goal4SmallGrid = world2grid(mapSmall, goal3World);
+planner4Small = plannerAStarGrid(mapSmall);
 
 tic;
-[path4_pequena_grid, debugInfo4_pequena] = plan(planner4_pequena, start4_pequena_grid, goal4_pequena_grid);
-tiempo_pequenas = toc;
-fprintf('Tiempo de cómputo para celdas PEQUEÑAS (0.02m): %.4f segundos\n', tiempo_pequenas);
+[path4SmallGrid, debugInfo4Small] = plan(planner4Small, start4SmallGrid, goal4SmallGrid);
+timeSmall = toc;
+fprintf('Tiempo de cómputo para celdas PEQUEÑAS (0.02m): %.4f s\n', timeSmall);
 
-path4_grande_world = grid2world(mapa_gran, path4_grande_grid);
-path4_pequena_world = grid2world(mapa_peq, path4_pequena_grid);
+% Plots
+path4LargeWorld = grid2world(mapLarge, path4LargeGrid);
+path4SmallWorld = grid2world(mapSmall, path4SmallGrid);
 
 figure;
 subplot(1,2,1);
-show(mapa_grande);
+show(mapLarge);
 hold on;
-if ~isempty(path4_grande_world)
-    plot(path4_grande_world(:,1), path4_grande_world(:,2), 'c-', 'LineWidth', 2);
-end
-title('Celdas Grandes (0.2m)');
+plot(path4LargeWorld(:,1), path4LargeWorld(:,2), 'c-', 'LineWidth', 2);
+title('Celdas grandes (0.2m)');
 
 subplot(1,2,2);
-show(mapa_pequena);
+show(mapSmall);
 hold on;
-if ~isempty(path4_pequena_world)
-    plot(path4_pequena_world(:,1), path4_pequena_world(:,2), 'y-', 'LineWidth', 2);
+plot(path4SmallWorld(:,1), path4SmallWorld(:,2), 'y-', 'LineWidth', 2);
+title('Celdas pequeñas (0.02m)');
+
+exportgraphics(gcf, '4_imagen.png', 'Resolution', 300);
+
+%% 5. Smoothed trajectory
+step = 8; 
+indices = 1:step:size(path3World, 1);
+if indices(end) ~= size(path3World, 1)
+    indices = [indices, size(path3World, 1)];
 end
-title('Celdas Pequeñas (0.02m)');
+controlPoints = path3World(indices, :);
 
-exportgraphics(gcf, 'actividad_4_imagen.png', 'Resolution', 300);
+t = 1:size(controlPoints, 1);
+tSmooth = 1:0.2:size(controlPoints, 1);
+pathSmoothX = spline(t, controlPoints(:,1), tSmooth);
+pathSmoothY = spline(t, controlPoints(:,2), tSmooth);
+pathSmooth = [pathSmoothX', pathSmoothY'];
 
-
-%% 5. Trayectoria suavizada
-paso = 8; 
-indices = 1:paso:size(path3_world, 1);
-
-if indices(end) ~= size(path3_world, 1)
-    indices = [indices, size(path3_world, 1)];
-end
-
-puntos_control = path3_world(indices, :);
-
-t = 1:size(puntos_control, 1);
-t_smooth = 1:0.2:size(puntos_control, 1);
-
-path_smooth_x = spline(t, puntos_control(:,1), t_smooth);
-path_smooth_y = spline(t, puntos_control(:,2), t_smooth);
-path_smooth = [path_smooth_x', path_smooth_y'];
-
-colisiones = checkOccupancy(mapa, path_smooth);
-num_colisiones = sum(colisiones);
+collisions = checkOccupancy(map, pathSmooth);
+numCollisions = sum(collisions);
 
 figure;
-show(mapa);
+show(map);
 hold on;
-h1 = plot(path3_world(:,1), path3_world(:,2), 'c-', 'LineWidth', 2.5);
-h2 = plot(path_smooth(:,1), path_smooth(:,2), 'y-', 'LineWidth', 2.5);
-h3 = plot(start3_world(1), start3_world(2), 'go', 'MarkerSize', 8, 'MarkerFaceColor', 'g');
-h4 = plot(goal3_world(1), goal3_world(2), 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r');
+h1 = plot(path3World(:,1), path3World(:,2), 'c-', 'LineWidth', 2.5);
+h2 = plot(pathSmooth(:,1), pathSmooth(:,2), 'y-', 'LineWidth', 2.5);
+h3 = plot(start3World(1), start3World(2), 'go', 'MarkerSize', 8, 'MarkerFaceColor', 'g');
+h4 = plot(goal3World(1), goal3World(2), 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r');
 
 title('5. Trayectoria suavizada');
 legend([h1, h2, h3, h4], 'Ruta original', 'Ruta suavizada', 'Start', 'Meta');
 
-save('actividad_5_resultado.mat', 'path_smooth');
-exportgraphics(gcf, 'actividad_5_imagen.png', 'Resolution', 300);
+save('5_resultado.mat', 'pathSmooth');
+exportgraphics(gcf, '5_imagen.png', 'Resolution', 300);
+fprintf('Ruta suavizada. Colisiones: %d\n', numCollisions);
 
-fprintf('Ruta suavizada. Colisiones: %d\n', num_colisiones);
-
-
-%% 6. Comparación con otro método (PRM - Probabilistic Roadmap)
+%% 6. Comparison with another method (PRM)
 prm = mobileRobotPRM;
-prm.Map = mapaInflada;
+prm.Map = mapInflated;
 prm.NumNodes = 120;
 prm.ConnectionDistance = 4;
 
-start6 = [1, 2];
-goal6 = [12, 12];
-
-path_prm = findpath(prm, start6, goal6);
+start6World = [1, 2];
+goal6World = [12, 12];
+pathPrm = findpath(prm, start6World, goal6World);
 
 figure;
 show(prm);
 hold on;
-if ~isempty(path_prm)
-    plot(path_prm(:,1), path_prm(:,2), 'm-', 'LineWidth', 3);
+if ~isempty(pathPrm)
+    plot(pathPrm(:,1), pathPrm(:,2), 'm-', 'LineWidth', 3);
 end
-plot(start6(1), start6(2), 'go', 'MarkerSize', 8, 'MarkerFaceColor', 'g');
-plot(goal6(1), goal6(2), 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r');
+plot(start6World(1), start6World(2), 'go', 'MarkerSize', 8, 'MarkerFaceColor', 'g');
+plot(goal6World(1), goal6World(2), 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r');
 
 title('6. Ruta obtenida con el algoritmo PRM');
-exportgraphics(gcf, 'actividad_6_imagen.png', 'Resolution', 300);
+exportgraphics(gcf, '6_imagen.png', 'Resolution', 300);
+
+%% --- HELPER FUNCTIONS ---
+function plotAndSave(mapObj, pathWorld, startW, goalW, titleStr, imgName, saveVarName, debugInfo)
+    % Helper function to plot maps, paths, and save results
+    figure;
+    show(mapObj);
+    hold on;
+    
+    if ~isempty(pathWorld)
+        h1 = plot(pathWorld(:,1), pathWorld(:,2), 'c-', 'LineWidth', 2);
+    else
+        h1 = [];
+    end
+    
+    h2 = plot(startW(1), startW(2), 'go', 'MarkerSize', 8, 'MarkerFaceColor', 'g');
+    h3 = plot(goalW(1), goalW(2), 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r');
+    
+    title(titleStr);
+    if ~isempty(h1)
+        legend([h1, h2, h3], 'Ruta A*', 'Start', 'Meta');
+    else
+        legend([h2, h3], 'Start', 'Meta');
+    end
+    
+    % Save .mat and .png
+    if exist('debugInfo', 'var')
+        save(sprintf('%s.mat', saveVarName), 'pathWorld', 'debugInfo');
+    else
+        save(sprintf('%s.mat', saveVarName), 'pathWorld');
+    end
+    
+    exportgraphics(gcf, imgName, 'Resolution', 300);
+    fprintf('Guardado imagen: %s\n', imgName);
+end
